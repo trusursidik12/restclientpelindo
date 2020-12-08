ca6ec2d52f6143d15b1f6fa5edeab24356702baa
trusursidik12 <trusursidik12@gmail.com>
2020-12-08 11:45:04 +0700

    A

--- application/config/config.php
+++ application/config/config.php
@@ -23,8 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/restclientpelindo';
-//  $config['base_url'] = 'http://ispumaps.id/restclientpelindo/';
+$config['base_url'] = 'http://ispumaps.id/restclientpelindo/';
 
 /*
 |--------------------------------------------------------------------------
